9776eefe1509f6e2aeaa631705fbd3697acef33d
2812pol <pol.melo14@gmail.com>
2025-10-21 13:09:23 +0200

    bootsrap until page 200

--- csshtml/AmaPolCV/main.js
+++ csshtml/AmaPolCV/main.js
@@ -3,3 +3,30 @@ function clickbuscar() {
 }
 
 
+ window.addEventListener('load',iniciar,false);
+
+	    /*Contador inicializado en cero*/
+	    var contador=0;
+
+	    function iniciar(){
+	        setInterval('cambiarImg()',5000);
+	    }
+
+	   	var obj=document.getElementById('slider');
+	    var obj2=obj.getElementsByTagName('img');
+
+    function cambiarImg(){	        
+
+        	if(contador==obj2.length){
+	            for(var i=0; i<obj2.length; i++){
+	                obj2[i].style.opacity='0';
+	                contador--;
+	            }
+	            obj2[contador].style.opacity='1';
+	        }
+	        else
+	        {
+		        obj2[contador].style.opacity='1';
+		        contador++;
+	        }
+        }
